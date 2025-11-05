b7a442df247598641f467d9aa811c4848b91af66
unnihari2332-cmd <unnihari2332@gmail.com>
2025-11-05 23:57:53 +0530

    Update About.js

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -35,10 +35,10 @@ const About = () => {
 
           <div className="infos">
             <p className="animated-layer fade-in-up-animation fadeInUp wow" data-wow-offset={200}>
-              <span>At <strong>Junior Odyssey International School (JOIS)</strong>, the <strong>Homer Eduship Curriculum</strong> is 
-              designed to nurture young learners in their most formative years.</span><span>Inspired by the UK’s 
-              <strong> Early Years Foundation Stage (EYFS)</strong> </span></span>and enriched with global best practices, it blends 
-              play, inquiry, and creativity with a strong academic foundation.</span>
+              <p>At <strong>Junior Odyssey International School (JOIS)</strong>, the <strong>Homer Eduship Curriculum</strong> is 
+              designed to nurture young learners in their most formative years.</p><p>Inspired by the UK’s 
+              <strong> Early Years Foundation Stage (EYFS)</strong> </p></p>and enriched with global best practices, it blends 
+              play, inquiry, and creativity with a strong academic foundation.</p>
             </p>
           </div>
         </div>
